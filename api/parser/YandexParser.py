106676a62cf9e98b22fa69b2b0e8ba06a0a4047c
asrom <asromahin@yandex.ru>
2020-02-16 23:47:49 +0700

    debug_grab_by_image

--- api/parser/YandexParser.py
+++ api/parser/YandexParser.py
@@ -93,7 +93,7 @@ class YandexParser():
         time.sleep(1)
         elem=self.wd.find_element_by_tag_name('li')
         elem = elem.find_element_by_tag_name('a')
-        start_url='https://yandex.ru'+elem.get_attribute('href')
+        start_url=elem.get_attribute('href')
         print('get url:',start_url)
         self.set_url(start_url)
         print('go to page')
