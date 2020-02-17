216970def8e47358564ce09bd42114dff125b5d4
asrom <asromahin@yandex.ru>
2020-02-17 21:25:13 +0700

    add parser by image url

--- api/parser/YandexParser.py
+++ api/parser/YandexParser.py
@@ -113,6 +113,7 @@ class YandexParser():
         time.sleep(1)
         print(f'set image url {image_url}')
         target_panel=self.wd.find_element_by_class_name('input__control')
+        target_panel.clear()
         target_panel.send_keys(image_url);
         self.wd.get_screenshot_as_file(save_screen)
         self.wd.find_element_by_class_name('cbir-panel__search-button').click()
