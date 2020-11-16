d59af6c8be6a00664f2b713d22b6c5557e5daf49
Grigory <barkev2009@bk.ru>
2020-11-16 08:26:51 +0300

    Rewrite README files

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import os
 
 YP = YandexParser('data')
 
-image_url = 'https://yandex.ru/images/search?rpt=imageview&url=https%3A%2F%2Favatars.mds.yandex.net%2Fget-images-cbir%2F2473989%2FxCvlKLoQ-M1xmxNwyLhtGQ%2Forig&cbir_id=2473989%2FxCvlKLoQ-M1xmxNwyLhtGQ&from=tabbar'
+image_url = ''       # Insert the link where Similar Pictures Block is depicted
 YP.get_by_image_url(image_url)
 
