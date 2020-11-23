23e89ac6e65db442940248c5fc7314a56ac7f3de
asrom <asromahin@yandex.ru>
2020-11-23 09:09:12 +0700

    fix

--- yparser/api/src/consts.py
+++ yparser/api/src/consts.py
@@ -1,4 +1,11 @@
 import os
 from yparser import chromedriver
+import sys
 
-CHROMEDRIVER_PATH = path = os.path.sep.join(os.path.abspath(chromedriver.__file__).split(os.path.sep)[:-1] + ['chromedriver'])
+
+if 'win' in sys.platform:
+    CHROMEDRIVER_PATH = os.path.sep.join(os.path.abspath(chromedriver.__file__).split(os.path.sep)[:-1] + ['chromedriver.exe'])
+else:
+    CHROMEDRIVER_PATH = os.path.sep.join(
+        os.path.abspath(chromedriver.__file__).split(os.path.sep)[:-1] + ['chromedriver']
+    )
