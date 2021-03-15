2b5cff798fd5541ddb2f0ce3e6cb7bfec575eceb
asrom <asromahin@yandex.ru>
2021-03-15 19:47:25 +0700

    'fix'

--- yparser/src/utils/colab_downloader.py
+++ yparser/src/utils/colab_downloader.py
@@ -1,4 +1,4 @@
-import sys
+import subprocess
 import os
 
 from yparser.src.consts import IN_COLAB
@@ -6,7 +6,9 @@ from yparser.src.consts import IN_COLAB
 
 def download_incolab_chromedriver():
     if IN_COLAB:
-        os.system('apt-get update')
-        os.system('apt install chromium-chromedriver')
+        p = subprocess.Popen(('apt-get', 'update'))
+        p.wait()
+        p = subprocess.Popen(('apt', 'install', 'chromium-chromedriver'))
+        p.wait()
         #os.system('cp /usr/lib/chromium-browser/chromedriver /usr/bin')
         #sys.path.insert(0, '/usr/lib/chromium-browser/chromedriver')
